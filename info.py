0c841d41359ee4bbfaf3b2991cd096881969adc4
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2024-01-07 14:14:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'atglinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'b9986d4349e6a50d21a06d33907902e0a93b31ea')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'JwJpMyTrUoMptmoacQytUNC3qRm2')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
